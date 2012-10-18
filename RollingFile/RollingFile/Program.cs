66c1e1a48d6ac3e794e23709a89e4e275bf50c2e
Don Huff <dhuffkc+github@gmail.com>
2012-10-18 08:36:09 -0500

    RollingFile test project.

---
+++ RollingFile/RollingFile/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace RollingFile
+{
+	using System;
+	using System.Threading;
+
+	using log4net;
+
+	class Program
+	{
+		private static readonly ILog Logger = LogManager.GetLogger(typeof(Program));
+		
+		static void Main(string[] args)
+		{
+			Logger.Info("Starting...");
+
+			while (true)
+			{
+				Logger.Debug("Jowl doner swine, ball tip short loin pork chuck frankfurter jerky beef ribs sirloin rump beef. Kielbasa pork spare ribs ham hock turducken tri-tip leberkas. Pastrami flank short loin spare ribs tail. Chicken venison pig leberkas. Chuck fatback short ribs, pork belly shoulder tail sausage salami short loin doner. at: " + DateTime.Now.ToLongTimeString());
+				Thread.Sleep(5);
+			}
+		}
+	}
+}
