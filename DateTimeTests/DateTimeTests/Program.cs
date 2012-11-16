37d34b6ab688232309c75fa79f704d89a99821d8
Don Huff <dhuffkc+github@gmail.com>
2012-11-16 16:28:47 -0600

    DateTimeTests solution.

---
+++ DateTimeTests/DateTimeTests/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace DateTimeTests
+{
+	using System;
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var d = new DateTime(1970, 1, 1);
+
+			var unspecified = new DateTime(d.Ticks, DateTimeKind.Unspecified);
+			var local = new DateTime(d.Ticks, DateTimeKind.Local);
+			var utc = new DateTime(d.Ticks, DateTimeKind.Utc);
+		}
+	}
+}
