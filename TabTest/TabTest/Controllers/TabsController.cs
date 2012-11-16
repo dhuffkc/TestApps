d7c8c975b21ed4cfd905edc9053ca48fbc0143cd
Don Huff <dhuffkc+github@gmail.com>
2012-11-16 16:30:24 -0600

    TabTest solution

---
+++ TabTest/TabTest/Controllers/TabsController.cs
@@ -0,0 +1,22 @@
+﻿namespace TabTest.Controllers
+{
+	using System.Web.Mvc;
+
+	public class TabsController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ActionResult DateContent()
+		{
+			return View();
+		}
+
+		public ActionResult LayoutContent()
+		{
+			return View();
+		}
+   }
+}
