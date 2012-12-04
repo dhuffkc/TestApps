b07d579e44870eeac4e0e2cb31355ae5afcef6f8
Don Huff <dhuffkc+github@gmail.com>
2012-12-04 16:16:23 -0600

    JavascriptPolling project

---
+++ JavascriptPolling/JavascriptPolling/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JavascriptPolling.MvcApplication" Language="C#" %>
---
+++ JavascriptPolling/JavascriptPolling/Scripts/Poller.js
@@ -0,0 +1,79 @@
+﻿Poller = function () {
+
+	var subscribers = [];
+	var timer;
+	var values = [];
+
+	var forcePoll = function () {
+		var isPolling = timer != undefined;
+
+		if (isPolling) {
+			stop();
+		}
+
+		handleUpdate();
+
+		if (isPolling) {
+			stop();
+		}
+	};
+
+	var handleUpdate = function () {
+		console.log("handleUpdate: " + new Date().toLocaleTimeString());
+		
+		var newValue = new Date();
+		var oldestTime = newValue.getTime() - 12 * 1000;
+
+		var deletedValues = values.filter(function (element) {
+			return element.getTime() < oldestTime;
+		});
+
+		values = values.filter(function (element) {
+			return element.getTime() >= oldestTime;
+		});
+
+		var addedValues = [newValue];
+
+		values = values.concat(addedValues);
+
+		subscribers.forEach(function (subscriber) {
+			subscriber(values, addedValues, deletedValues);
+		});
+	};
+
+	var start = function () {
+		handleUpdate();
+		timer = setTimeout(start, 5000);
+	};
+
+	var stop = function () {
+		if (timer != undefined) {
+			clearTimeout(timer);
+			timer = undefined;
+		}
+	};
+
+	var subscribe = function (callback) {
+		if (callback != undefined) {
+			subscribers.push(callback);
+			return callback;
+		}
+
+		return null;
+	};
+
+	var unsubscribe = function (subscription) {
+		subscribers = subscribers.filter(function (element) {
+			return element !== subscription;
+		});
+	};
+
+	return {
+		forcePoll: forcePoll
+		, start: start
+		, stop: stop
+		, subscribe: subscribe
+		, unsubscribe: unsubscribe
+		, values: values
+	};
+};
