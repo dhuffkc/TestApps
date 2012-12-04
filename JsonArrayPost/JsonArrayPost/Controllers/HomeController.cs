17c37ed3e28490476ada0abfebe301ac4d1d812c
Don Huff <dhuffkc+github@gmail.com>
2012-12-04 16:19:49 -0600

    JsonArrayPost project

---
+++ JsonArrayPost/JsonArrayPost/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿
+namespace JsonArrayPost.Controllers
+{
+	using System.Collections.Generic;
+	using System.Web.Mvc;
+	
+	public class Item
+	{
+		public string Data { get; set; }
+	}
+
+	public class PostData
+	{
+		public IList<string> Test { get; set; }
+	}
+
+	public class HomeController : Controller
+	{
+		[HttpPost]
+		public ActionResult PostTest(IList<string> stuff)
+		{
+			return this.Json("awesome!");
+		}
+
+		public ActionResult Index()
+		{
+			ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+			return View();
+		}
+
+		public ActionResult About()
+		{
+			return View();
+		}
+	}
+}
