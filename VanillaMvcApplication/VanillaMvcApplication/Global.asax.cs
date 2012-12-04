70a8865c780d3fa56c11efbb198aa8c58a623c9b
Don Huff <dhuffkc+github@gmail.com>
2012-12-04 16:20:31 -0600

    VanillaMvcApplication project
    Test for unhandled exception logging.

---
+++ VanillaMvcApplication/VanillaMvcApplication/Global.asax.cs
@@ -0,0 +1,57 @@
+﻿
+namespace VanillaMvcApplication
+{
+	using System;
+	using System.Web;
+	using System.Web.Mvc;
+	using System.Web.Routing;
+	using log4net;
+
+	// Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+	// visit http://go.microsoft.com/?LinkId=9394801
+
+	public class MvcApplication : System.Web.HttpApplication
+	{
+		private static readonly ILog Logger = LogManager.GetLogger(typeof(MvcApplication));
+
+		public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+		{
+			filters.Add(new LogErrorAttribute());
+		}
+
+		public static void RegisterRoutes(RouteCollection routes)
+		{
+			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+			routes.MapRoute(
+				"Default", // Route name
+				"{controller}/{action}/{id}", // URL with parameters
+				new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+			);
+
+		}
+
+		protected void Application_Start()
+		{
+			Logger.Debug("Application Start");
+
+			AreaRegistration.RegisterAllAreas();
+
+			RegisterGlobalFilters(GlobalFilters.Filters);
+			RegisterRoutes(RouteTable.Routes);
+		}
+
+		protected void Application_Error(object sender, EventArgs e)
+		{
+			var message = new
+				{
+					Location = "Application_Error",
+					HttpContext = HttpContext.Current
+				};
+
+			var exception = Server.GetLastError();
+
+			Logger.Error(message, exception);
+		}
+	}
+}
---
+++ VanillaMvcApplication/VanillaMvcApplication/LogErrorAttribute.cs
@@ -0,0 +1,18 @@
+﻿
+namespace VanillaMvcApplication
+{
+	using System.Web.Mvc;
+
+	using log4net;
+
+	public class LogErrorAttribute : HandleErrorAttribute
+	{
+		private static readonly ILog Logger = LogManager.GetLogger(typeof(LogErrorAttribute));
+
+		public override void OnException(ExceptionContext filterContext)
+		{
+			Logger.Error("Unhandled exception", filterContext.Exception);
+			base.OnException(filterContext);
+		}
+	}
+}
